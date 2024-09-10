bf3484fe193fce1df967319cd63ea61599870b4e
jordan.CD <jordancdiaz04@gmail.com>
2024-09-10 00:33:10 -0400

    basic json validation for /register

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,26 @@
-import dotenv from "dotenv";
 import express from "express";
-
-dotenv.config();
+import { userCredentialsSchema } from "./schemas";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
-  console.log("get home route");
+  res.send("Express + TypeScript Server hello");
+  console.log("GET request from home route received.");
 });
+
+app.post("/register", (req, res) => {
+  const { body } = req
+  const validBody = userCredentialsSchema.safeParse(body)
+  if(!validBody.success || validBody.error) {
+    res.status(400).send(JSON.stringify({errorMessage: "Invalid JSON Body"}))
+    return
+  }
+  console.log(`we have a good json where username = ${validBody.data.username} and password = ${validBody.data.password}`);
+})
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
