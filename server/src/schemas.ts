f95cb4d6f2c4b3f99b66f5421fd48b944bb6c995
jordan.CD <jordancdiaz04@gmail.com>
2024-09-13 02:08:14 -0400

    /login redirects if auth_token present

--- server/src/schemas.ts
+++ server/src/schemas.ts
@@ -4,4 +4,8 @@ export const userCredentialsSchema = z.object({
     username: z.string().min(1),
     password: z.string().min(1),
   });
+
+export const authCookiesSchema = z.object({
+  auth_token: z.string().min(1),
+})
   
