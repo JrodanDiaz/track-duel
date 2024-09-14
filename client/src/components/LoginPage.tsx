d04534e1a0cec0a50a6e2c904b58bfcfb161d4fa
jordan.CD <jordancdiaz04@gmail.com>
2024-09-14 03:45:12 -0400

    log in to spotify redirect

--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { UserCredentials } from "../types";
 import { LoginUser } from "../api/auth";
 import { useUserContext, useUserDispatchContext } from "./UserContext";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
 
@@ -27,10 +28,32 @@ export default function LoginPage() {
     })
   };
 
+  const handleSpotifyClick = async () => {
+    const spotifyClientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+    if (!spotifyClientId) throw new Error("Spotify Client Id is undefined")
+      
+    const redirect_url = "http://localhost:3000/auth/callback"
+    const scope = "streaming user-read-email user-read-private user-read-playback-state";
+
+    const auth_query_params = new URLSearchParams({
+        response_type: "code",
+        client_id: spotifyClientId,
+        scope: scope,
+        redirect_uri: redirect_url,
+    });
+
+  console.log(`spotifyLoginHandler: Redirecting to callback`);
+  console.log(`auth query params: ${auth_query_params}`);
+  
+    
+  window.location.href = "https://accounts.spotify.com/authorize/?" + auth_query_params.toString()
+  // navigate("https://accounts.spotify.com/authorize/?" + auth_query_params.toString())
+  }
+
   return (
     <>
       <div className="border-black border-[1px] rounded-sm m-4 p-4 w-full justify-center items-center">
-        <form
+      {!userContext.username ? <form
           action=""
           className="flex flex-col justify-center items-center"
           onSubmit={handleSubmit}
@@ -57,7 +80,35 @@ export default function LoginPage() {
           >
             Submit
           </button>
-        </form>
+        </form> : <><button onClick={handleSpotifyClick} className="p border-black border-[1px] bg-green-600 text-black px-3 py-1">Authorize Spotify</button></>} 
+        {/* <form
+          action=""
+          className="flex flex-col justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="border-black border-[1px] px-3 py-1"
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter Username"
+            onChange={handleChange("username")}
+          />
+          <input
+            className="border-black border-[1px] px-3 py-1"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter Password"
+            onChange={handleChange("password")}
+          />
+          <button
+            type="submit"
+            className="border-black border-[1px] px-4 py-2 bg-slate-400"
+          >
+            Submit
+          </button>
+        </form> */}
       </div>
     </>
   );
