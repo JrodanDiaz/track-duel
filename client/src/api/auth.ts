93f8a1c93713191599bc3ab75d843d321eb2f425
jordan.CD <jordancdiaz04@gmail.com>
2024-10-11 01:50:24 -0400

    Register and Login are now identically typed

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -44,7 +44,7 @@ export async function LoginUser(user: UserCredentials) {
   const userData = implicitLoginSchema.safeParse(await response.json())
   if(!userData.success) throw new Error("Internal Server Error")
   if("errorMessage" in userData.data) throw new Error(userData.data.errorMessage)
-  return userData.data
+  return userData.data.auth_token
 }
 
 export const getToken = async (): Promise<string> => {
