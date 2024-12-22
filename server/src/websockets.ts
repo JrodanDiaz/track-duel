81d806c5672c7136169b9006b47c67d18d91e622
jordan.CD <jordancdiaz04@gmail.com>
2024-12-22 18:02:45 -0500

    improve readability in websockets.ts

--- server/src/websockets.ts
+++ server/src/websockets.ts
@@ -2,13 +2,27 @@ import { IncomingMessage, Server } from "http";
 import WebSocket, { WebSocketServer } from "ws";
 import { rooms, userRoomMap } from "./handlers/roomHandler";
 
-const getUserFromRequest = (req: IncomingMessage) => {
-    const path = "http://localhost:3000"
-    const url = new URL(path + req.url)
-    return url.searchParams.get("user")
+const userSocketMap = new Map<WebSocket, string>();
+
+const sendMessage = (ws: WebSocket, message: Object) => {
+    ws.send(JSON.stringify(message))
 }
 
-const userSocketMap = new Map<WebSocket, string>()
+const getUserFromRequest = (req: IncomingMessage) => {
+    const path = "http://localhost:3000";
+    const url = new URL(path + req.url);
+    return url.searchParams.get("user");
+};
+
+const sendMessageToRoom = (roomCode: string, message: Object) => {
+    rooms[roomCode].users.forEach((socket) => {
+        socket.send(JSON.stringify(message));
+    });
+};
+
+const getUsersFromRoom = (roomCode: string) => {
+    return Array.from(rooms[roomCode].users).map((socket) => userSocketMap.get(socket));
+};
 
 export const configureWebsocketServer = (server: Server) => {
     const wss = new WebSocketServer({ server });
@@ -19,47 +33,41 @@ export const configureWebsocketServer = (server: Server) => {
 
     wss.on("connection", (ws, req) => {
         console.log(`Websocket Connection established...`);
-        let roomCode: string | null = null
-        const user = getUserFromRequest(req)
-        if(!user) {
-            ws.close()
+        let roomCode: string | null = null;
+        const user = getUserFromRequest(req);
+        if (!user) {
+            ws.close();
             console.log(`User Undefined. Closing connection`);
-            return 
+            return;
         }
-        userSocketMap.set(ws, user)
+        userSocketMap.set(ws, user);
 
         ws.on("error", onSocketPostError);
 
         ws.on("message", (msg, isBinary) => {
             console.log(`Received Websocket Message: ${msg.toString()}`);
-            const parsedMessage = JSON.parse(msg.toString())
+            const parsedMessage = JSON.parse(msg.toString());
 
-            if (parsedMessage.type === 'join-room' && parsedMessage.roomCode) {
+            if (parsedMessage.type === "join-room" && parsedMessage.roomCode) {
                 //disgusting type cast to stop TS from crying. Is Zod validation really necessary
                 roomCode = parsedMessage.roomCode as string;
                 if (rooms[roomCode]) {
-                  rooms[roomCode].users.add(ws);  
-                  console.log(`User ${user} joined room ${roomCode}`);
+                    rooms[roomCode].users.add(ws);
+                    console.log(`User ${user} joined room ${roomCode}`);
 
-                  const lobby = Array.from(rooms[roomCode].users).map(socket => userSocketMap.get(socket));
+                    const lobby = getUsersFromRoom(roomCode);
 
-                  console.log(`${roomCode} Lobby: ${lobby}`);
+                    console.log(`${roomCode} Lobby: ${lobby}`);
 
-                  ws.send(JSON.stringify({ type: 'room-joined', users: lobby }));
+                    sendMessage(ws, {type: "room-joined", users: lobby})
 
-                  rooms[roomCode].users.forEach(socket => {
-                    socket.send(JSON.stringify({type: "user-joined", user: user}))
-                  })
+                    sendMessageToRoom(roomCode, { type: "user-joined", user: user });
                 } else {
-                  ws.send(JSON.stringify({ type: 'error', message: 'Room not found' }));
+                    sendMessage(ws, {type: "error", message: "Room not found"})
                 }
-              }
-            else if(parsedMessage.type === "start-duel" && parsedMessage.roomCode) {
-                rooms[parsedMessage.roomCode].users.forEach(socket => {
-                    socket.send(JSON.stringify({type: "start-duel"}))
-                })
-            }
-            else {
+            } else if (parsedMessage.type === "start-duel" && parsedMessage.roomCode) {
+                sendMessageToRoom(parsedMessage.roomCode, {type: "start-duel"})
+            } else {
                 wss.clients.forEach((client) => {
                     if (client.readyState === WebSocket.OPEN) {
                         client.send(msg, { binary: isBinary });
@@ -69,42 +77,28 @@ export const configureWebsocketServer = (server: Server) => {
         });
 
         ws.on("close", () => {
-            console.log("Connection closed");
+            console.log("Connection closed...");
+            userSocketMap.delete(ws);
 
-            
-            if(roomCode && rooms[roomCode]) {
-                console.log(`Room Before: ${rooms[roomCode].users.size}`);
+            if (roomCode && rooms[roomCode]) {
 
-                rooms[roomCode].users.delete(ws)
+                rooms[roomCode].users.delete(ws);
+                console.log(`Removed ${user} from room ${roomCode}`);
 
-                console.log(`Room After: ${rooms[roomCode].users.size}`);
-                if(rooms[roomCode].users.size === 0) {
-                    console.log(`Deleting room ${roomCode}`);
-                    delete rooms[roomCode]
-                }
-                else {
-                    const room = Array.from(rooms[roomCode].users).map(socket => userSocketMap.get(socket));
-                    rooms[roomCode].users.forEach(socket => {
-                        socket.send(JSON.stringify({type: "room-update", room: room}))
-                    })
+                if (rooms[roomCode].users.size === 0) {
+                    delete rooms[roomCode];
+                    console.log(`Deleted empty room ${roomCode}`);
+                } else {
+                    const room = getUsersFromRoom(roomCode);
+                    sendMessageToRoom(roomCode, { type: "room-update", room: room });
                 }
             }
-            if(user && userRoomMap[user]) {
-                console.log(`userRoomMap before DC: ${JSON.stringify(userRoomMap)}`);
-                delete userRoomMap[user]
-                console.log(`userRoomMap after DC: ${JSON.stringify(userRoomMap)}`);
-                
+            if (userRoomMap[user]) {
+                delete userRoomMap[user];
+                console.log(`Deleted user's UserRoomMap: ${JSON.stringify(userRoomMap)}`);
             }
 
-            // if(userSocketMap.has(ws)) {
-            console.log(`userSocketMap Size Before: ${userSocketMap.size}`);
-            userSocketMap.delete(ws)
-            console.log(`userSocketMap Size After: ${userSocketMap.size}`);
-            // }
-
-            if(user) {
-                console.log(`${user} disconnected...`);
-            }
+            console.log(`${user} disconnected...`);
         });
     });
 };
