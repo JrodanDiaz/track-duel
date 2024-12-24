ecdcb79b72a1ced43bdc9639b3f7eeba959cf1e0
jordan.CD <jordancdiaz04@gmail.com>
2024-12-24 18:34:19 -0500

    make ListPlaylist component more readable

--- client/src/components/duel/ListPlaylist.tsx
+++ client/src/components/duel/ListPlaylist.tsx
@@ -1,5 +1,6 @@
 import { skipToken } from "@reduxjs/toolkit/query";
 import { useGetPlaylistMinimumQuery } from "../../store/api/playlistsApiSlice";
+import ErrorMessage from "../common/ErrorMessage";
 
 interface Props {
     uri: string;
@@ -26,32 +27,38 @@ export default function ListPlaylist({
 }: Props) {
     const { data, isLoading, error } = useGetPlaylistMinimumQuery(uri ?? skipToken);
 
-    if (isLoading) return <div className=" text-lilac">Loading playlist...</div>;
+    if (isLoading) return <SkeletonListPlaylist />;
 
     if (error || data === undefined)
-        return <div className=" text-red-500">Error fetching playlist</div>;
+        return <ErrorMessage message="Error fetching playlist" />;
+
+    const selectedTextStyle = () => {
+        return selectedIndex === index ? "text-black" : "text-offwhite";
+    };
+
+    const hoverTextStyle = () => {
+        return selectedPlaylistUri !== uri ? "group-hover:text-main-green" : "";
+    };
+
+    const handleClick = () => {
+        if (setSelectedIndex && index !== undefined) {
+            setSelectedIndex(index === selectedIndex ? null : index);
+        }
+    };
 
     return (
         <div
             className={`flex items-center justify-between pr-8 cursor-pointer border-[1px] border-y-offwhite/50 bg-transparent group ${
                 selectedIndex === index && "!bg-main-green"
             } ${classname}`}
-            onClick={() => {
-                if (setSelectedIndex && index !== undefined) {
-                    setSelectedIndex(index === selectedIndex ? null : index);
-                }
-            }}
+            onClick={handleClick}
         >
             <div className="flex gap-4 items-center">
                 {data.images[0].url && (
                     <img src={data.images[0].url} height={imageSize} width={imageSize} />
                 )}
                 <p
-                    className={`${
-                        selectedIndex === index ? "text-black" : "text-offwhite"
-                    } text-2xl font-kanit ${
-                        selectedPlaylistUri !== uri && "group-hover:text-main-green"
-                    }`}
+                    className={`${selectedTextStyle()} ${hoverTextStyle()} text-2xl font-kanit `}
                 >
                     {data.name}
                 </p>
@@ -69,3 +76,12 @@ export default function ListPlaylist({
         </div>
     );
 }
+
+const SkeletonListPlaylist = () => {
+    return (
+        <div className=" flex items-center gap-4">
+            <div className=" bg-gray-300 animate-pulse duration-500 h-[110px] w-[110px] border-y-offwhite/50"></div>
+            <p className="text-offwhite text-2xl font-kanit">Loading...</p>
+        </div>
+    );
+};
